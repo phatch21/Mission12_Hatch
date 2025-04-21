58b98f92e9ffd16fe6a83d5ca53da2396c502864
phatch21 <phatch21@byu.edu>
2025-04-21 12:58:09 -0600

    Fixed mime

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,13 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target:
-          "https://asdf-e4h9gqa5g0akf3d4.westus2-01.azurewebsites.net", // Your backend
-        changeOrigin: true,
-        secure: false,
-      },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"), // ✅ critical for Azure
     },
   },
 });
